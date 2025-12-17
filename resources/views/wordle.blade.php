7e043fd57e050b17c2c16384fef9c332a13b5f98
owenhuis <143698007+owenhuis@users.noreply.github.com>
2025-12-17 11:28:55 +0100

    wordle begonnen invoeren van woord en controle gemaakt

---
+++ resources/views/wordle.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Wordle</h1>
+
+<form method="POST">
+    @csrf
+    <input type="text" name="guess" maxlength="1" required>
+    <input type="text" name="guess2" maxlength="1" required>
+    <input type="text" name="guess3" maxlength="1" required>
+    <input type="text" name="guess4" maxlength="1" required>
+    <input type="text" name="guess5" maxlength="1" required>
+    <button type="submit">Submit</button>
+</form>
+
+<?php
+$wordToGuess = "ABCDE";
+$result = "ik werk niet";
+echo $_SERVER['REQUEST_METHOD'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo "post werkt";
+    if (strlen($userGuess) !== 5) {
+        $result = "Please enter exactly 5 letters.";
+    } elseif (strtoupper($userGuess) === $wordToGuess) {
+        $result = "Congratulations! You've guessed the word!";
+    } else {
+        $result = "Incorrect guess. Try again!";
+    }
+    
+}
+echo "<p>" . $result . "</p>";
+echo $_SERVER['REQUEST_METHOD'];
+?>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('test');
+    return view('wordle');
+});
+
+Route::post('/', function (Request $request) {
+    $userGuess =
+    request('guess') .
+    request('guess2') .
+    request('guess3') .
+    request('guess4') .
+    request('guess5');
+
+    return view('wordle') -> with('userGuess', $userGuess);
 });
